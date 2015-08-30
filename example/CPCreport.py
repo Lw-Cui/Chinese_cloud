880365e3363b77d2bd3b1db3a41faf422af59e08
Lw-Cui <645739460@QQ.com>
2015-08-30 15:46:19 +0800

    upload to pipy

--- example/CPCreport.py
+++ example/CPCreport.py
@@ -5,5 +5,5 @@ from chinese_cloud.chinese_cloud import ChineseCloud
 
 text_dir = path.dirname(__file__)
 #text = open(path.join(text_dir, 'constitution.txt')).read()
-text = open(path.join(text_dir, 'article')).read()
+text = open(path.join(text_dir, 'CPCreport.txt')).read()
 chinese_cloud = ChineseCloud(width=800, height=400, max_font=100, min_font=20).generate(text).to_image('report.png')
