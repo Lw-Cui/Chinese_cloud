63e65900dbb5927901dd5872ed94b25d0e51333c
Lw-Cui <645739460@QQ.com>
2015-09-05 08:48:46 +0800

    add font

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from distutils.extension import Extension
 
 setup(
     name='chinese_cloud',
-    version='1.0',
+    version='1.1',
     ext_modules=[Extension("chinese_cloud.query_integral_image",
                            ["chinese_cloud/query_integral_image.c"])],
     packages=['chinese_cloud'],
-    package_data={'chinese_cloud': ['stopwords']}
+    package_data={'chinese_cloud': ['stopwords', 'DroidSansFallbackFull.ttf']}
 )
